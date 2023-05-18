ba5ef1f489ee040daf639c72174b9016a32d3693
Marcos Faquete <anttonimarco@gmail.com>
2023-05-17 22:57:36 -0300

    Script Shell

---
+++ Shell Script/teste.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+
+True = 1
+False = 2
+
+while [$True == 1]
+do
+    echo "<<< Cadastro >>>"
+    sleep 1
+done
+
+echo "Qual é o seu nome?"
+read nome
+
+echo "Qual é sua idade?"
+read idade
+
+echo "Seu nome é: $nome. "
+sleep 1
+
+echo "Sua idade é: $idade anos."
+
+echo "Olá"
+sleep 1 
+
+echo "Olá"
+sleep 1
+
+echo "Olá"
+sleep 1
+
