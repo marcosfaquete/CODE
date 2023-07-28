132fc43d4fc611e34d930e70a2022fbe4cd30118
Antônio Marcos <anttonimarco@gmail.com>
2023-07-27 22:32:08 -0300

    Program

--- C#/Projeto Music/Program.cs
+++ C#/Projeto Music/Program.cs
@@ -1,6 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-Console.WriteLine(mensagemDeBoasVindas);
 
+void ExibirMensagemDeBoasVindas()
+{
+
+    Console.WriteLine(@"
+█▀ █▀▀ █▀█ █▀▀ █▀▀ █▄░█   █▀ █▀█ █░█ █▄░█ █▀▄
+▄█ █▄▄ █▀▄ ██▄ ██▄ █░▀█   ▄█ █▄█ █▄█ █░▀█ █▄▀
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+
+
+
+}
+
+void ExibirOpcoesDoMenu()
+{
+    
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair\n");
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
